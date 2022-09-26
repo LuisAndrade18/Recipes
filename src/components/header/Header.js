50b945678861f7bf95b9a2d1df9a1ae212cfcdf2
dheniffe <dheniffe.oliveira@gmail.com>
2022-09-26 00:39:31 -0300

    create header and latestRecipes mobile section

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import MenuIcon from "../../assets/icons/menuIcon.svg";
+import * as S from "./Style.js";
 
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <S.Header>
+      <nav>
+        <S.BoxMenuHamburger>
+          <S.MenuHamburger src={MenuIcon} alt="Menu hamburger" />
+          <S.MenuMobileContent>
+            <S.BoxMobileList>
+              <S.MenuMobileItem>ABOUT</S.MenuMobileItem>
+              <S.MenuMobileItem>RECIPES</S.MenuMobileItem>
+              <li>
+                <S.SubscribesButton>SUBSCRIBES</S.SubscribesButton>
+              </li>
+            </S.BoxMobileList>
+          </S.MenuMobileContent>
+        </S.BoxMenuHamburger>
+      </nav>
+      <S.BoxTitle>
+        <S.Title>RECIPES</S.Title>
+      </S.BoxTitle>
+    </S.Header>
   );
 }
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import LatestRecipes from "./components/latestRecipes/LatestRecipes";
+import About from './components/about/About'
+import * as S from "./Style.js";
 
 export default function Main() {
   return (
-    <div>
-      <h1>Main</h1>
-    </div>
+    <S.Main>
+      <LatestRecipes />
+      <About/>
+    </S.Main>
   );
 }
