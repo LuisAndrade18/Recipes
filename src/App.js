2319752cdfa4fb99827a6acc6bc4e3affbf31928
dheniffe <dheniffe.oliveira@gmail.com>
2022-09-27 22:40:13 -0300

    remove pull updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
-import Footer from "./components/footer/Footer";
 import * as S from "./theme/GlobalStyle";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <S.GLobalStyle />
       <Header />
       <Main />
-      <Footer />
     </div>
   );
 }
